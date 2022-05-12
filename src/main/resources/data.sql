164ad58c4d000a171fed6b2bf1c4b101e3305844
brascat <catarina.bras@diconium.com>
2022-05-12 12:48:22 +0100

    Fix database initializaion error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+INSERT INTO category (category_name) VALUES ('American');
 INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
 INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description) VALUES ('Cup');
