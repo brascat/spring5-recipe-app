da98ac43122ba6bc6754717ed1242f274940fe6c
brascat <catarina.bras@diconium.com>
2022-05-13 13:43:18 +0100

    Configure recipes controller

--- src/main/java/guru/springframework/spring5recipeapp/controllers/indexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/indexController.java
@@ -4,7 +4,9 @@ import guru.springframework.spring5recipeapp.domain.Category;
 import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
 import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import guru.springframework.spring5recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,23 +14,29 @@ import java.util.Optional;
 @Controller
 public class indexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public indexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public indexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"/", "", "/index"})
     public String getIndexPage() {
 
-        Optional<Category> categoryOptional = categoryRepository.findByCategoryName("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        //Optional<Category> categoryOptional = categoryRepository.findByCategoryName("American");
+        //Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 
-        System.out.println("Cat ID is " + categoryOptional.get().getId());
-        System.out.printf("UoM ID is " + unitOfMeasureOptional.get().getId());
+        //System.out.println("Cat ID is " + categoryOptional.get().getId());
+        //System.out.printf("UoM ID is " + unitOfMeasureOptional.get().getId());
 
         return "index";
+    }
+
+    @RequestMapping("/recipes")
+    public String getRecipesPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getAllRecipes());
+
+        return "recipes";
     }
 }
--- src/main/java/guru/springframework/spring5recipeapp/service/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/service/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package guru.springframework.spring5recipeapp.service;
 
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -17,8 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public Set<Recipe> getAllRecipes() {
         Set<Recipe> recipes = new HashSet<>();
-        //recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
-        //return recipeRepository.getAll();
-        return null;
+        recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
+        return recipes;
     }
 }
