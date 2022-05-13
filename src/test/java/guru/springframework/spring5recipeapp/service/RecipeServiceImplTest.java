27005fd8121250943d323c94f1edcf57e5f365a2
brascat <catarina.bras@diconium.com>
2022-05-13 16:06:48 +0100

    Add getAllRecipes test

---
+++ src/test/java/guru/springframework/spring5recipeapp/service/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring5recipeapp.service;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getAllRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getAllRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
