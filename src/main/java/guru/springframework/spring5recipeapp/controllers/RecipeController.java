8227a8f583a6a22839368fda423e562bd4bbd5d5
brascat <catarina.bras@diconium.com>
2022-05-16 16:11:22 +0100

    Add view to add a recipe

--- src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.commands.RecipeCommand;
 import guru.springframework.spring5recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import static java.lang.Long.parseLong;
 
@@ -17,9 +17,25 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipes/show/{id}")
+    @RequestMapping("/recipes/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(parseLong(id)));
         return "recipes/show";
     }
+
+    @RequestMapping("/recipes/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipes/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipes/show/" + savedCommand.getId();
+    }
+
 }
